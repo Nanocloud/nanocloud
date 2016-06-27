00f927a6469c18caa9e8cfbfb7b8d9530c1a440a
Olivier Berthonneau <berthonneau.olivier@gmail.com>
2016-06-27 22:19:54 +0200

    Remove auto generated description in User model

--- api/models/User.js
+++ api/models/User.js
@@ -1,12 +1,5 @@
 var bcrypt = require("bcryptjs");
 
-/**
- * User.js
- *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
- */
-
 module.exports = {
 
   attributes: {
