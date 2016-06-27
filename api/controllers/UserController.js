a72360142866bd44dbeb3f96db4bd17f361b815d
Olivier Berthonneau <berthonneau.olivier@gmail.com>
2016-06-27 22:21:37 +0200

    Return current user if request if GET /users?me=true

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -5,7 +5,24 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
-module.exports = {
-	
-};
+var JSONAPISerializer = require('json-api-serializer');
+var Serializer = new JSONAPISerializer();
+var findRecords = require('sails-json-api-blueprints/lib/api/blueprints/find');
 
+module.exports = {
+
+  find: function(req, res) {
+
+    if (req.allParams()['me'] === "true") {
+      Serializer.register('users', {
+        id: 'id'
+      });
+
+      var me = Serializer.serialize('users', req.user);
+
+      return res.send(me);
+    }
+
+    return findRecords(req, res);
+  }
+};
