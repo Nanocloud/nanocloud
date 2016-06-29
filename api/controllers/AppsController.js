59bbd302312c973394518d093b8da0ba53ed54b5
Olivier Berthonneau <berthonneau.olivier@gmail.com>
2016-06-29 21:26:02 +0200

    sails generate api apps

---
+++ api/controllers/AppsController.js
@@ -0,0 +1,11 @@
+/**
+ * AppsController
+ *
+ * @description :: Server-side logic for managing apps
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
