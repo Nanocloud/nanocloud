2756ddfb15d7426296f5d957c16abbf7d92315f4
Romain Soufflet <romain.soufflet@nanocloud.com>
2016-07-25 19:12:41 +0200

    Add licence text into frontend files.

--- assets/app/components/vdi-drag-n-drop/component.js
+++ assets/app/components/vdi-drag-n-drop/component.js
@@ -1,3 +1,27 @@
+/**
+ * Nanocloud, a comprehensive platform to turn any application into a cloud
+ * solution.
+ *
+ * Copyright (C) 2016 Nanocloud Software
+ *
+ * This file is part of Nanocloud.
+ *
+ * Nanocloud is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Nanocloud is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General
+ * Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
 import Ember from 'ember';
 
 /* global $:false */
@@ -20,7 +44,7 @@ var FileUploader = Ember.Object.extend(Ember.Evented, {
 
     this.set('req', req);
 
-    req.open('POST', '/upload?filename=' + encodeURIComponent(this.get('file').name));
+    req.open('POST', '/api/upload?filename=' + encodeURIComponent(this.get('file').name));
     req.setRequestHeader('Authorization', 'Bearer ' + this.get('token'));
 
     req.upload.onprogress = (event) => {
