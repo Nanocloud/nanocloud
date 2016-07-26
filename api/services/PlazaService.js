605d8495d4cb3ee626a89f9a3bc9cff60887b1f5
Antoine Leblanc <antoine.leblanc@nanocloud.com>
2016-07-26 16:56:26 +0200

    Add Plaza service.

---
+++ api/services/PlazaService.js
@@ -0,0 +1,59 @@
+const http = require("http")
+
+module.exports = {
+
+  exec: function(hostname, cmd, stdin) {
+    let data = {
+      "command": cmd,
+      "stdin": stdin
+    };
+
+    let options = {
+      host: hostname,
+      path: '/exec',
+      port: '9090',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST'
+    }
+
+    let req = http.request(options, function (response) {
+      console.log(response.statusCode);
+      console.log(response.body);
+    });
+
+    req.write(JSON.stringify(data));
+    req.end();
+  },
+
+  files: function(hostname, filename, path, next) {
+    let options = {
+      host: hostname,
+      path: "/files?path=" + path,
+      port: '9090',
+      method: 'GET'
+    }
+
+    let req = http.request(options, function(response) {
+      response.on('data', function (data) {
+        next(JSON.parse(data));
+      });
+    });
+
+    req.end();
+  },
+
+  upload: function(hostname, filename, path) {
+    let options = {
+      host: hostname,
+      path: "/upload?filename" + filename,
+      port: 9090,
+      method: 'POST'
+    }
+
+    let req = http.request(options, function(response) {
+      console.log(response.body);
+    });
+  }
+};
