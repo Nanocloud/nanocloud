ddf44b3456faa35d02cfea28811e6fd250b10877
Romain Soufflet <romain.soufflet@nanocloud.com>
2016-07-26 10:14:49 +0200

    Add a script to check or add licences in source code files.

--- add_licences.sh
+++ add_licences.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 LICENSE_TEXT="/**
- * Nanocloud, a comprehensive platform to turn any application into a cloud
- * solution.
+ * Nanocloud turns any traditional software into a cloud solution, without
+ * changing or redeveloping existing source code.
  *
  * Copyright (C) 2016 Nanocloud Software
  *
