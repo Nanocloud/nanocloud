ba8d5c2f0934bc18b81c9c4a062f319bfb93bba6
Antoine Leblanc <antoine.leblanc@nanocloud.com>
2016-07-28 17:16:24 +0200

    Add api test for storage.

---
+++ tests/api/storage.test.js
@@ -0,0 +1,105 @@
+/**
+ * Nanocloud turns any traditional software into a cloud solution, without
+ * changing or redeveloping existing source code.
+ *
+ * Copyright (C) 2016 Nanocloud Software
+ *
+ * This file is part of Nanocloud.
+ *
+ * Nanocloud is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Nanocloud is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General
+ * Public License
+ * along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+// jshint mocha:true
+
+const nano = require('./lib/nanotest');
+const expect = require('chai').expect;
+
+module.exports = function() {
+
+  describe("Storage", function() {
+    const fileSchema = {
+      type: 'object',
+      properties: {
+        'mod_time': {type: 'number'},
+        'name': {type: 'string'},
+        'size': {type: 'number'},
+        'type': {type: 'string'},
+      },
+      required: ['mod_time', 'name', 'size', 'type'],
+      additionalProperties: false
+    };
+
+    let filename = 'storage.test.js';
+    let fileSize = null;
+
+
+    describe("Upload a file", function() {
+      it('Should upload a file', function(done) {
+        nano.request(sails.hooks.http.app)
+          .post('/api/upload?filename=' + filename)
+          .attach(filename, './tests/api/storage.test.js', filename)
+          .set(nano.adminLogin())
+          .expect(200)
+          .end(done);
+      });
+    });
+
+    describe("Get user's files", function() {
+      it('Should return a list of files', function(done) {
+        nano.request(sails.hooks.http.app)
+          .get('/api/files')
+          .set(nano.adminLogin())
+          .expect(200)
+          .expect(nano.jsonApiSchema(fileSchema))
+          .expect((res) => {
+            fileSize = res.body.data[0].attributes.size;
+          })
+          .end(done);
+      });
+    });
+
+    describe("Download a file", function() {
+
+      let downloadToken = null;
+      describe("Get a download token", function() {
+        it('Should return a download token', function(done) {
+          nano.request(sails.hooks.http.app)
+            .get('/api/files/token?filename=' + filename)
+            .set(nano.adminLogin())
+            .expect(200)
+            .expect((res) => {
+              downloadToken = res.body.token;
+            })
+            .end(done);
+        });
+      });
+
+      describe("Download file", function() {
+        it('Should return file content', function(done) {
+          nano.request(sails.hooks.http.app)
+            .get('/api/files/download?filename=' + filename + '&token=' + downloadToken)
+            .expect(200)
+            .expect('Content-Type', 'application/javascript')
+            .expect('Content-disposition', 'attachment; filename="' + filename + '"')
+            .expect((res) => {
+              expect(res.text.length).to.equal(fileSize);
+            })
+            .end(done);
+        });
+      });
+    });
+  });
+};
