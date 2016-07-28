c70b8f1fc1de30ddb49c62b052abb0a1e833f413
Eric Cao <eric.cao@nanocloud.com>
2016-07-28 17:57:13 +0200

    add license and fix jshint errors

--- api/controllers/PendingUserController.js
+++ api/controllers/PendingUserController.js
@@ -1,9 +1,35 @@
 /**
- * PendingUserController
+ * Nanocloud turns any traditional software into a cloud solution, without
+ * changing or redeveloping existing source code.
  *
- * @description :: Server-side logic for managing pendingusers
+ * Copyright (C) 2016 Nanocloud Software
+ *
+ * This file is part of Nanocloud.
+ *
+ * Nanocloud is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * Nanocloud is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ConfigController
+ *
+ * @description :: Server-side logic for managing configs
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
+
+/* globals ConfigService */
+/* globals EmailService */
+/* globals PendingUser */
+/* globals JsonApiService */
+/* globals User */
 
 const uuid = require('node-uuid');
 
@@ -22,7 +48,7 @@ module.exports = {
       var to =  user.email;
       var subject = 'Nanocloud - Verify your email address';
       var message = 'Hello ' + user["first-name"] + ' ' + user["last-name"] + ',<br> please verify your email address by clicking this link: '+
-          '<a href="'+configuration.host+'/#/activate/'+user.id+'">Activate my account</a>';
+          '<a href="'+host+'/#/activate/'+user.id+'">Activate my account</a>';
 
       EmailService.sendMail(to, subject, message)
       .then(() => {
@@ -30,7 +56,7 @@ module.exports = {
       })
       .then((created_user) => {
         return res.created(created_user);
-      })
+      });
     })
     .catch((err) => {
       return res.negotiate(err);
@@ -54,9 +80,9 @@ module.exports = {
         })
       .then(() => {
         return res.ok(user);
-      })
+      });
     })
-    .catch((error) => {
+    .catch(() => {
       return res.notFound('An error has occured while retrieving user');
     });
   } 
