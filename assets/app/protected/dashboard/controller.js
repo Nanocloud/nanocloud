86ce3eb23b1602f94a713843c7eda4629a1b6457
Eric Cao <eric.cao@nanocloud.com>
2016-08-17 14:23:34 +0200

    machines up were not displayed in dashboard

--- assets/app/protected/dashboard/controller.js
+++ assets/app/protected/dashboard/controller.js
@@ -52,7 +52,7 @@ export default Ember.Controller.extend({
 
   machines: Ember.computed('model.machines', 'model.machines', function() {
     var machines = this.get('model.machines')
-      .filterBy('status', 'up');
+      .filterBy('status', 'RUNNING');
     return machines;
   }),
 
