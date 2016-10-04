d8f11e9c00699893fecaa2dac623b3b6605e9440
Eric Cao <eric.cao@nanocloud.com>
2016-10-04 15:46:24 +0200

    jshint

--- assets/app/components/application-publisher/component.js
+++ assets/app/components/application-publisher/component.js
@@ -26,8 +26,8 @@ import Ember from 'ember';
 import fileExplorer from 'nanocloud/components/file-explorer/component';
 import focusInputsHandlerMixin from 'nanocloud/mixins/focus-inputs-handler';
 
-export default fileExplorer.extend(
-  focusInputsHandlerMixin, {
+export default fileExplorer.extend(focusInputsHandlerMixin, {
+
   publishError: false,
 
   loadDirectory() {
@@ -48,24 +48,24 @@ export default fileExplorer.extend(
 
     this.set('isPublishing', true);
     m.save()
-      .then(() => {
-        this.set('isPublishing', false);
-        this.sendAction('action');
-        window.swal({
-          title: 'Success!',
-          text: 'Your application has been onboarded.',
-          type: 'success',
-          showCancelButton: false,
-          confirmButtonText: 'Great.',
-          closeOnConfirm: true,
-          animation: false
-        });
-      }, (error) => {
-        this.set('isPublishing', false);
-        this.set('publishError', true);
-        this.set('selectedFile', null);
-        this.toast.error(error.errors[0].status + ' : ' + error.errors[0].title);
+    .then(() => {
+      this.set('isPublishing', false);
+      this.sendAction('action');
+      window.swal({
+        title: 'Success!',
+        text: 'Your application has been onboarded.',
+        type: 'success',
+        showCancelButton: false,
+        confirmButtonText: 'Great.',
+        closeOnConfirm: true,
+        animation: false
       });
+    }, (error) => {
+      this.set('isPublishing', false);
+      this.set('publishError', true);
+      this.set('selectedFile', null);
+      this.toast.error(error.errors[0].status + ' : ' + error.errors[0].title);
+    });
   },
 
   selectFile(file) {
