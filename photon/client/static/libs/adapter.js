c6d6fa428f623fbb711eaaa00c26b3088f13ddb8
Olivier Berthonneau <berthonneau.olivier@gmail.com>
2016-10-11 11:41:42 +0200

    Ignore Photon's client libs

--- photon/client/static/libs/adapter.js
+++ photon/client/static/libs/adapter.js
@@ -1,21 +1,7 @@
-/**
- * Nanocloud turns any traditional software into a cloud solution, without
- * changing or redeveloping existing source code.
+/*
+ *  Copyright (c) 2014 The WebRTC project authors. All Rights Reserved.
  *
- * Copyright (C) 2016 Nanocloud Software
- *
- * This file is part of Nanocloud.
- *
- * Nanocloud is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * Nanocloud is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
  */
