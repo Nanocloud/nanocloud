521696eb27e182e82a138e75bac2df29d5d6578a
Olivier Berthonneau <berthonneau.olivier@gmail.com>
2016-10-11 11:41:42 +0200

    Ignore Photon's client libs

--- photon/client/static/libs/adapter.js
+++ photon/client/static/libs/adapter.js
@@ -1,30 +1,17 @@
-/**
- * Nanocloud turns any traditional software into a cloud solution, without
- * changing or redeveloping existing source code.
+/*
+ *  Copyright (c) 2014 The WebRTC project authors. All Rights Reserved.
  *
- * Copyright (C) 2016 Nanocloud Software
- *
- * This file is part of Nanocloud.
- *
- * Nanocloud is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * Nanocloud is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  Use of this source code is governed by a BSD-style license
+ *  that can be found in the LICENSE file in the root of the source
+ *  tree.
  */
 
+/* More information about these options at jshint.com/docs/options */
 /* jshint browser: true, camelcase: true, curly: true, devel: true,
    eqeqeq: true, forin: false, globalstrict: true, node: true,
-   quotmark: single, undef: true, unused: strict */
+ quotmark: single, undef: true, unused: strict */
 /* global mozRTCIceCandidate, mozRTCPeerConnection, Promise,
-mozRTCSessionDescription, webkitRTCPeerConnection, MediaStreamTrack */
+ mozRTCSessionDescription, webkitRTCPeerConnection, MediaStreamTrack */
 /* exported trace,requestUserMedia */
 
 'use strict';
