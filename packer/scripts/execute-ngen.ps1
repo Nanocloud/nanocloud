18fb1ad200b1056ead0f9c1ac973a12d4175b1b9
Antoine Leblanc <Leblantoine@users.noreply.github.com>
2016-10-13 17:53:20 +0200

    Execute ngen to compile .NET assemblies. (#366)

---
+++ packer/scripts/execute-ngen.ps1
@@ -0,0 +1,4 @@
+C:\Windows\microsoft.net\framework\v4.0.30319\ngen.exe update /force /queue
+C:\Windows\microsoft.net\framework64\v4.0.30319\ngen.exe update /force /queue
+C:\Windows\microsoft.net\framework\v4.0.30319\ngen.exe executequeueditems
+C:\Windows\microsoft.net\framework64\v4.0.30319\ngen.exe executequeueditems
